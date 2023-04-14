9161b2d7d82c680e629e190f72440c7f5f0d91d2
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-04-14 15:17:09 +0530

    new

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Tokenomics from "./components/Tokenomics";
 function App() {
   return (
     <div className="w-screen h-auto overflow-x-hidden">
-      <div className="h-[auto] bg-[url('../public/assets/images/bg-main-full.png')] bg-cover">
+      <div className="h-[auto] bg-[url('../public/assets/home.png')] bg-cover pb-60">
         <div className=" mx-auto  max-w-7xl px-3 md:px-10  ">
           <Navbar />
           <Main />
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,15 +4,15 @@ const Main = () => {
   return (
     <div className="grid mt-10 grid-cols-5 w-full pb-16 place-items-start">
       <div className="flex h-full flex-col justify-between">
-        <img src="/assets/images/fox1.png" className="h-auto w-full"/>
-        <img src="/assets/images/fox2.png" className="h-auto w-full"/>
+        <img src="/assets/images/fox1.png" className="h-auto w-full" alt=""/>
+        <img src="/assets/images/fox2.png" className="h-auto w-full" alt=""/>
       </div>
       <div className="col-span-3">
-        <img src="/assets/images/main-fox.png" className="h-60 sm:h-[650px] w-auto self-center" />
+        <img src="/assets/images/main-fox.png" className="h-60 sm:h-[650px] w-auto self-center" alt="" />
       </div>
       <div className="flex h-full flex-col justify-between">
-        <img src="/assets/images/fox3.png" className="h-auto w-full"/>
-        <img src="/assets/images/fox4.png" className="h-auto w-full"/>
+        <img src="/assets/images/fox3.png" className="h-auto w-full" alt=""/>
+        <img src="/assets/images/fox4.png" className="h-auto w-full" alt=""/>
       </div>
     </div>
   );
