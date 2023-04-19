9606d52bbb7debb004d27238052f05a0dc0f60da
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-04-19 08:47:35 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,17 +7,13 @@ function Footer() {
       <div className="mt-20 flex flex-col">
         <div className="px-5 mx-auto max-w-[30rem] w-full  flex-col ">
           <div className="flex justify-between sm:justify-center w-full items-center">
-            <img src="/assets/logos/logo.svg" alt=""/>
+            <img src="/assets/logos/logo.svg" alt="" className="flex sm:hidden"/>
             <div className="flex sm:hidden text-white">
               <FaTelegramPlane className="mr-3" />
               <FaTwitter />
             </div>
           </div>
-          <div className="mt-7 flex justify-between items-center flex-wrap text-center font-orbitron text-white text-sm sm:text-lg">
-            <a href="#about">About Us</a>
-            <a href="#token">Tokenomics</a>
-            <a href="#roadmap">Roadmap</a>
-          </div>
+       
         </div>
         <img src="/assets/images/line.png" className="w-full flex h-auto my-16" alt="" />
         <div className="mb-7 flex justify-between items-center flex-wrap font-orbitron text-[#CCD2E9] text-[9px] sm:text-base">
