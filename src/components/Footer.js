de29c26e8383459c047a265d498a8b54a639f462
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-04-19 08:53:30 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ function Footer() {
       
       <div className="mt-20 flex flex-col">
         <div className="px-5 mx-auto max-w-[30rem] w-full  flex-col ">
-          <div className="flex justify-between sm:justify-center w-full items-center">
-            <img src="/assets/logos/logo.svg" alt="" className="flex sm:hidden"/>
+          <div className="flex justify-center sm:justify-center w-full items-center">
+            <img src="/assets/logos/logo.svg" alt="" className="hidden"/>
             <div className="flex sm:hidden text-white">
               <FaTelegramPlane className="mr-3" />
               <FaTwitter />
@@ -15,8 +15,8 @@ function Footer() {
           </div>
        
         </div>
-        <img src="/assets/images/line.png" className="w-full flex h-auto my-16" alt="" />
-        <div className="mb-7 flex justify-between items-center flex-wrap font-orbitron text-[#CCD2E9] text-[9px] sm:text-base">
+        <img src="/assets/images/line.png" className="w-full flex h-auto my-2" alt="" />
+        <div className="mb-1 flex justify-center items-center flex-wrap font-orbitron text-[#CCD2E9] text-[9px] sm:text-base">
           <div className="hidden sm:flex text-white text-lg">
             <FaTelegramPlane className="mr-3" />
             <FaTwitter />
